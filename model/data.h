aa55b9432b2aca0e102b4806a47bf5645de6969b
David Kennedy S. A <daviduser.guj@gmail.com>
2017-05-07 04:59:48 -0300

    Add business data structure.

---
+++ model/data.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <list>
+#include <string>
+
+using std::string;
+using std::wstring;
+using std::list;
+
+using Level = unsigned int;
+
+namespace adopt_a_freshman {
+
+struct SocialMedia
+{
+    wstring fbhook;
+    wstring gphook;
+};
+
+struct Habits {
+    list<wstring> recreations;
+    list<wstring> entertainments;
+    list<wstring> disciplines;
+};
+
+enum Category { VETERAN, FRESHMAN };
+
+struct Profile
+{
+    wstring name;
+    wstring university;
+    wstring field;
+    string telephone;
+    string avatar;
+    SocialMedia user_medias;
+
+    Level level;
+    Category type;
+};
+
+struct UserRegistry
+{
+    string username;
+    string password;
+    int token;
+    Profile profile;
+};
+
+struct Adoption {
+  Profile veteran;
+  Profile freshman;
+};
+
+list<UserRegistry> users;
+list<Adoption> adoptions, adoption_requests_by_freshman, adoption_request_by_veteran;
+int token=0;
+
+list<wstring> entertainments = { 
+  L"Dota", 
+  L"LoL", 
+  L"GoT" 
+};
+
+list<wstring> disciplines = { 
+  L"Cálculo", 
+  L"Física", 
+  L"Geometria", 
+  L"Literatura", 
+  L"Algebra", 
+  L"Programação" 
+};
+
+list<wstring> university = {};
+list<wstring> fields = {};
+
+struct Report {
+  string title;
+  string description;
+};
+
+list<Report> bug_reports;
+
+} /* adopt_a_freshman */ 
